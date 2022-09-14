c04e6c37b43cd62f85b53edd7bf701d19c860e8a
Hoàng Văn Tuyến <tuyenhv@tienngay.vn>
2022-09-14 22:58:35 +0700

    Bài 63: Dựng khung Layout mặc định

---
+++ src/components/Layout/components/Header/index.js
@@ -0,0 +1,14 @@
+import style from './Header.module.scss';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(style);
+
+function Header() {
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('inner')}></div>
+        </header>
+    );
+}
+
+export default Header;
