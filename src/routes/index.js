570b0a74d674baaae722f980af32f3397656f17f
Hoàng Văn Tuyến <tuyenhv@tienngay.vn>
2022-11-24 21:58:54 +0700

    Bai 73 Logic Header 2

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,7 @@ import Home from '~/pages/Home';
 import Following from '~/pages/Following';
 import Profile from '~/pages/Profile';
 import Upload from '~/pages/Upload';
+import Search from '~/pages/Search';
 
 //public routes
 const publicRoutes = [
@@ -17,13 +18,18 @@ const publicRoutes = [
         component: Following,
     },
     {
-        path: '/profile',
+        path: '/@:nickname',
         component: Profile,
     },
     {
         path: '/upload',
         component: Upload,
         layout: HeaderOnly,
+    },
+    {
+        path: '/search',
+        component: Search,
+        layout: null,
     },
 ];
 
